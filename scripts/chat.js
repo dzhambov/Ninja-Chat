e7b04ecaf7594ac9c0be8b9231dbaa36dfbefb5d
Hristo <owner@iMac.local>
2021-01-04 16:41:55 -0800

    add real time listener.

--- scripts/chat.js
+++ scripts/chat.js
@@ -24,10 +24,22 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+
+  getChats(callback) {
+    this.chats
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if(change.type === 'added') {
+            // update the UI
+            callback(change.doc.data());
+          }
+        });
+    });
+  }
 }
 
 const chatroom = new Chatroom('gaming','fei');
 
-chatroom.addChats('hello gang')
-.then(() => console.log('chat added'))
-.catch(err => console.log('err'));
+chatroom.getChats((data) => {
+  console.log(data);
+});
