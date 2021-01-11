285cc20d0d2a16926b1eb171feb0a549659ca1e0
Hristo <owner@iMac.local>
2021-01-11 12:40:38 -0800

    add new chats functionality.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,15 @@
 // DOM queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
 
+// Add a new chat
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChats(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err))
+});
 // Class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'fei');
