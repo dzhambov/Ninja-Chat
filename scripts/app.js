27fe0bcd0f748d2defec2120ecba0c81725e41fd
Hristo <owner@iMac.local>
2021-01-11 13:06:10 -0800

    add changing the name and local storage.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // DOM queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMessage = document.querySelector('.update-mssg');
 
 // Add a new chat
 newChatForm.addEventListener('submit', e => {
@@ -10,9 +12,27 @@ newChatForm.addEventListener('submit', e => {
     .then(() => newChatForm.reset())
     .catch(err => console.log(err))
 });
+
+// Update Username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  //update name via chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  //reset the form
+  newNameForm.reset();
+  // show then hide the update message
+  updateMessage.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => updateMessage.innerText = '', 3000);
+});
+
+// Check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anon'
+
+
 // Class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'fei');
+const chatroom = new Chatroom('general', username);
 
 //Get chats and render
 chatroom.getChats(data => chatUI.render(data));
